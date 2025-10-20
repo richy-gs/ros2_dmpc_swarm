4afd2204c6b92defd71818bce9c7e4603851cebe
richy-gs <jesusrg2405@gmail.com>
2025-10-20 11:47:56 -0600

    New launch and rviz files

--- src/mpc_drone_viz/setup.py
+++ src/mpc_drone_viz/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'mpc_drone_viz'
 
@@ -10,6 +12,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        # Include all launch files
+        (os.path.join('share', package_name, 'launch'),
+         glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        # Include all RViz files
+        (os.path.join('share', package_name, 'rviz'),
+         glob(os.path.join('rviz', '*.rviz'))),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
